583a5f092755c42cf76e1ffdb512cc1880fa85a6
cuttage <martafattori@protonmail.ch>
2022-10-09 11:23:16 +0200

    Google Form Tooltip update

--- src/components/GoogleForm.js
+++ src/components/GoogleForm.js
@@ -4,7 +4,7 @@ const GoogleForm = () => {
   const formUrl =
     'https://docs.google.com/forms/d/e/1FAIpQLSf4CELDYZ0QVGZjmwCKWgxdo97oi4tmSkyclxPrenXPHjTMxw/viewform'
   return (
-    <Tooltip title="Google Form">
+    <Tooltip title="Google Form - Sign in required">
       <a
         href={formUrl}
         rel="noopener noreferrer"
